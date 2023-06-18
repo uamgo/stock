4097c6de865c1db99ae296c3966863a753c87b5d
kevin <kaihua.xu@esgyn.cn>
2023-06-18 10:27:56 +0800

    add timing

--- main.py
+++ main.py
@@ -153,36 +153,44 @@ class StockA:
             self.lock.release()
 
     def run_thread_check(self, codes):
+        s_ts = time.time()
+        s_date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         thread_rs = list()
         thx_name = current_thread().getName()
         code_len = len(codes)
-        print(f"[{thx_name}] total len: {code_len}")
+        # print(f"[{thx_name}] total len: {code_len}")
         n = 0
+        flag = False
         for row in codes:
             code = row['code']
             n += 1
-            if n % 100 == 0:
-                print(f"[{thx_name}] finished {n}/{code_len}")
-            # print(code)
+            if n % 200 == 0:
+                now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                print(f"[{thx_name}] finished {n}/{code_len} with time: {now}")
             if self.filter_by_day(code, 5) and self.filter_by_min(code):
                 thread_rs.append(row)
-                print(f'{row}')
+                flag = True
+                print(f'[{thx_name}] {row}')
                 print('-------------------\n')
-        print(f"[{thx_name}] finished all: {code_len}")
-        if len(thread_rs) > 0:
+        e_ts = "%.2f" % (time.time() - s_ts)
+        e_date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        if flag:
+            print(f"[{thx_name}] finished all: {code_len}, elapse: {e_ts} ({s_date}  ~  {e_date})")
             self.append_all(thread_rs)
 
     def run(self):
+        s_ts = time.time()
         stock_zh_a_spot_em_df = ak.stock_zh_a_spot_em()
-        stock_df_len = len(stock_zh_a_spot_em_df)
-        print(f"Total stocks: {stock_df_len}")
-        # not_st_df = stock_zh_a_spot_em_df.loc['ST' not in stock_zh_a_spot_em_df['名称'] and '退市' not in stock_zh_a_spot_em_df['名称'] and 'N' not in stock_zh_a_spot_em_df['名称'], ['名称']]
+        # not_st_df = stock_zh_a_spot_em_df.loc['ST' not in stock_zh_a_spot_em_df['名称']
+        # and '退市' not in stock_zh_a_spot_em_df['名称'] and 'N' not in stock_zh_a_spot_em_df['名称'], ['名称']]
         thread_data_dict = dict()
         thread_dict = dict()
         for i in range(1, 50):
             thread_data_dict[i] = list()
         batch_num = 1
+        batch_size = 100
         n = 0
+        v_n = 0
         for i in stock_zh_a_spot_em_df.index:
             code = stock_zh_a_spot_em_df['代码'][i]
             n += 1
@@ -203,6 +211,9 @@ class StockA:
 
             if code.startswith("688"):
                 continue
+
+            v_n += 1
+
             shi_val = stock_zh_a_spot_em_df['市盈率-动态'][i]
             total_val = stock_zh_a_spot_em_df['总市值'][i]
             row = dict()
@@ -214,20 +225,23 @@ class StockA:
             thread_data_dict[batch_num].append(row)
             rape_list = list()
             rape_list.append(thread_data_dict[batch_num])
-            if len(thread_data_dict[batch_num]) >= 100:
+            if len(thread_data_dict[batch_num]) >= batch_size:
                 thread_dict[batch_num] = Thread(target=self.run_thread_check, args=rape_list, name=batch_num,
                                                 daemon=True)
                 thread_dict[batch_num].start()
-                print(f"Valid stock code: {batch_num*100}/{n}")
+                # print(f"Valid stock code: {batch_num*batch_size}/{n}")
                 batch_num += 1
 
         if len(thread_data_dict[batch_num]) > 0:
             thread_dict[batch_num] = Thread(target=self.run_thread_check, args=rape_list, name=batch_num, daemon=True)
             thread_dict[batch_num].start()
 
+        # stock_df_len = len(stock_zh_a_spot_em_df)
+        e_ts = "%.2f" % (time.time() - s_ts)
+        print(f"Batch size: {batch_size}, num of batches:{batch_num}, total of stock codes: {v_n}/{n}, Elapse:{e_ts}\n")
         for k in thread_dict:
             thread_dict[k].join(timeout=3600000)
-            print(f"Thread {k} done! ---")
+            # print(f"Thread {k} done! ---")
         return self.rs
 
 
@@ -238,7 +252,8 @@ if __name__ == "__main__":
     rs.sort(key=lambda r: r['code'])
     end_ts = time.time()
 
+    print("\n~~~~~~~~~~~\n")
     for idx, row in enumerate(rs):
         print(
-            f"{str(idx)}, code={row['code']}, name={row['name']}, 市盈率-动态={row['shi_val']}, 总市值={row['total_val']}, 流通市值={row['flow_val']}")
-    print("Elapse: %.2f s" % (end_ts - start_ts))
+            f"{str(idx+1)}, code={row['code']}, name={row['name']}, 市盈率-动态={row['shi_val']}, 总市值={row['total_val']}, 流通市值={row['flow_val']}")
+    print("\nElapse: %.2f s,  %s" % ((end_ts - start_ts), time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
